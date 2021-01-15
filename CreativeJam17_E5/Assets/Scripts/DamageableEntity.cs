e285ba666f5cf4566595441e976ef6e52bcae378
iMeteox <meteoxtheforgotten@hotmail.fr>
2021-01-15 18:40:47 -0500

    Damageable Entity Script

---
+++ CreativeJam17_E5/Assets/Scripts/DamageableEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableEntity : MonoBehaviour
+{
+    [SerializeField] private float MaxHealth;
+
+    private float currentHealth;
+    private bool isAlive;
+
+    public Action OnDeath;
+    public Action OnRevive;
+       
+    private void Awake()
+    {
+        currentHealth = MaxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        // Take damage
+        currentHealth = Math.Max(0, (currentHealth - damage));
+
+        // If life drops to or under 0, die
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Revive()
+    {
+        // Prepare character for revive
+        isAlive = true;
+        currentHealth = MaxHealth;
+
+        // Broadcast the revive event
+        OnRevive.Invoke();
+    }
+
+    private void Die()
+    {
+        // Prepare character for death
+        isAlive = false;
+
+        // Broadcast the death event
+        OnDeath.Invoke();
+    }
+}
