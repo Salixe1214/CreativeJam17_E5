16e94cc80c1762d33256fed4073bdfd8c5e8c67d
iMeteox <meteoxtheforgotten@hotmail.fr>
2021-01-16 11:40:56 -0500

    Ghost works! Ayy!

---
+++ CreativeJam17_E5/Assets/Scripts/Weapon/LingeringAttack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LingeringAttack : MonoBehaviour
+{
+    [SerializeField] public float Damage;
+    [SerializeField] public float HitboxRefreshSeconds;
+
+    private Collider2D attackCollider;
+
+    private void Awake()
+    {
+        attackCollider = GetComponent<Collider2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Is it damageable?
+        DamageableEntity damageable = collision.gameObject.GetComponent<DamageableEntity>();
+        if (damageable)
+        {
+            // Deal your damage
+            damageable.TakeDamage(Damage);
+
+            // Have a refresh system so you can take damage many times if he's over you all the time
+            StartCoroutine(RefreshHitbox());
+        }
+    }
+
+    public IEnumerator RefreshHitbox()
+    {
+        float endTime = Time.time + HitboxRefreshSeconds;
+        attackCollider.enabled = false;
+
+        while (endTime > Time.time)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        attackCollider.enabled = true;
+        yield return null;
+    }
+}
