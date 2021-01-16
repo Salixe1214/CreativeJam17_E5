757952ef3e309969e773bad51732e66f8ad6f552
MelpyX <maxime.gaudet.1@ulaval.ca>
2021-01-16 14:39:05 -0500

    debut de ramassable

--- CreativeJam17_E5/Assets/Scripts/Ramassable.cs
+++ CreativeJam17_E5/Assets/Scripts/Ramassable.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Ramassable : MonoBehaviour
 {
+    public GameObject player;
     enum ramassables {bonusTemps, lance, epee, lancePierre};
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.GetComponent<PlayerDetector>().OnPlayerEnter += fonctionPasFaite;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void fonctionPasFaite(GameObject player)
+    {
+
     }
 }
