08a95e6bf3ac1207c6874a1ebe1c12cecba95285
MelpyX <maxime.gaudet.1@ulaval.ca>
2021-01-17 15:36:13 -0500

    Game end UI

--- CreativeJam17_E5/Assets/Scripts/UI/BarreDeTemps.cs
+++ CreativeJam17_E5/Assets/Scripts/UI/BarreDeTemps.cs
@@ -7,8 +7,6 @@ public class BarreDeTemps : MonoBehaviour
 {
     public GameObject player;
     Image barreDeTemps;
-    public Text texte;
-    string txt = "";
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +20,6 @@ public class BarreDeTemps : MonoBehaviour
         float resetTimer = player.GetComponent<ResetTimer>().resetTimer;
         float tempsAjouteEphemere = player.GetComponent<ResetTimer>().tempsAjouteEphemere;
         float tempsDeLaSession = player.GetComponent<ResetTimer>().tempsDeLaSession;
-
-        
-        Debug.Log(1 - (tempsDeLaSession / resetTimer * player.GetComponent<statisticsGestion>().getTimerModif() + tempsAjouteEphemere) + 0.087f);
         barreDeTemps.fillAmount = (1-(tempsDeLaSession / (resetTimer * player.GetComponent<statisticsGestion>().getTimerModif() + tempsAjouteEphemere))+0.087f);
-
-        //txt = (1 - (tempsDeLaSession / resetTimer * player.GetComponent<statisticsGestion>().getTimerModif() + tempsAjouteEphemere) + 0.087f).ToString();
-        txt = (resetTimer * player.GetComponent<statisticsGestion>().getTimerModif() + tempsAjouteEphemere).ToString();
-        texte.GetComponent<UnityEngine.UI.Text>().text = txt;
-
-
     }
 }
