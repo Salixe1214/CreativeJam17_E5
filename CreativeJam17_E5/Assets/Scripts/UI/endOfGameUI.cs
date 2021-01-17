0d26d3853d4f01d94b0b3dbdfaa47dc3eb82af93
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2021-01-17 12:46:16 -0500

    Menu de fin avec infos

--- CreativeJam17_E5/Assets/Scripts/UI/endOfGameUI.cs
+++ CreativeJam17_E5/Assets/Scripts/UI/endOfGameUI.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class endOfGameUI : MonoBehaviour
 {
     [SerializeField] Text nbDeathUI;
+    [SerializeField] Text timeText;
+    float timeFloat;
 
     // Start is called before the first frame update
     void Awake()
     {
         nbDeathUI.text = PlayerPrefs.GetInt("nbDeath").ToString();
+        timeFloat = PlayerPrefs.GetFloat("timeGame");
+
+        timeText.text = ((int)timeFloat).ToString() + "s";
     }
 
     // Update is called once per frame
